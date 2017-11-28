7f83782f1d5a48968cfa20328f46870f81487f00
이현우 <sincerehw@hotmail.com>
2017-11-28 15:46:00 +0900

    참조 정리

--- ChromeSelenium/RunningAdOrder.py
+++ ChromeSelenium/RunningAdOrder.py
@@ -1,9 +1,3 @@
-
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from WebFroxy import WebFroxy
 
 def doOrder():
